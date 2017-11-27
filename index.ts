635ba7fd4edc4fb2cc84a69649d1ea37bee52888
Menno <menno.vrahden@gmail.com>
2017-11-27 18:12:13 +0100

    add index

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { Solver } from './src/Solver';
+import { DQNSolver } from './src/DQNSolver';
+import { Env } from './src/Env';
+import { Opt } from './src/Opt';
+
+export { Solver, DQNSolver, Env, Opt };
