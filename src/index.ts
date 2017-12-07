120f39bf9dfd1f23db6c648156f4adca68196c7e
Menno <menno.vrahden@gmail.com>
2017-12-07 23:06:05 +0100

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Solver } from './Solver';
-import { DQNSolver } from './dqn/DQNSolver';
 import { Env } from './Env';
 import { Opt } from './Opt';
+import { DQNSolver } from './dqn/DQNSolver';
+import { DQNOpt } from './dqn/DQNOpt';
 
-export { Solver, DQNSolver, Env, Opt };
+export { Solver, Env, Opt, DQNSolver, DQNOpt };
