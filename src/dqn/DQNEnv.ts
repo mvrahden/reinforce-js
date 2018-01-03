0d658916a19b45296e8c77d48c6bcd6c357d86b5
Menno <menno.vrahden@gmail.com>
2018-01-03 14:44:33 +0100

    update

---
+++ src/dqn/DQNEnv.ts
@@ -0,0 +1,3 @@
+import { Env } from '../index';
+
+export class DQNEnv extends Env {}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Solver } from './Solver';
 import { Env } from './Env';
 import { Opt } from './Opt';
+
 import { DQNSolver } from './dqn/DQNSolver';
 import { DQNOpt } from './dqn/DQNOpt';
+import { DQNEnv } from './dqn/DQNEnv';
+
 import { TDSolver } from './td/TDSolver';
 import { TDOpt } from './td/TDOpt';
 import { TDEnv } from './td/TDEnv';
 
-export { Solver, Env, Opt, DQNSolver, DQNOpt, TDSolver, TDOpt, TDEnv };
+export { Solver, Env, Opt, DQNSolver, DQNOpt, DQNEnv, TDSolver, TDOpt, TDEnv };
